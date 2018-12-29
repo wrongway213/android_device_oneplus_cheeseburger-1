c2d463003c89dfec92f34fc147221e888dd4f732
martinusbe <martinusbe@gmail.com>
2018-12-29 14:32:05 +0100

    Revert "cheeseburger: add maintainers back"
    This reverts commit d2399f7077f50f0b3711977279eb7ab2ef293a4b.

--- aicp_cheeseburger.mk
+++ aicp_cheeseburger.mk
@@ -42,8 +42,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := OnePlus/OnePlus5/OnePlus5:9/PKQ1.180716.001/1812232046:user/release-keys
 
-# AICP Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="Carlos Solano (csolanol), SpiritCroc"
-
 TARGET_VENDOR := oneplus
